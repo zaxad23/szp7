07b5b36cd058fd3866f9f2447427b0a5050bd7f9
zaxad23 <adamek23wp.pl@wp.pl>
2017-02-14 22:00:20 +0100

    big update

---
+++ profile/baza.js
@@ -0,0 +1,37 @@
+var baza = {};
+
+baza.members = [
+	"skoorupa",
+	"сочное яблоко",
+	"Mathio",
+	"Adam10lol",
+	"Scorpion",
+	"Czarny",
+	"EmloX",
+	"Oryginalny nick",
+	"ShaQii"
+];
+
+baza.link = [
+	"skoorupa",
+	"76561198240826178",
+	"xMathiox",
+	"76561198188622762",
+	"76561198196601947",
+	"76561198217839261",
+	"emlox",
+	"76561198355460339",
+	"76561198314272042"
+];
+
+baza.vlink = [
+	"id/", 
+	"profiles/",
+	"id/",
+	"profiles/",
+	"profiles/",
+	"profiles/",
+	"id/",
+	"profiles/",
+	"profiles/"
+];
